04d25aabc13b499611765ad61e97021a907b2219
Danny Nguyen <danhn142@gmail.com>
2016-06-28 16:38:01 -0700

    update completeish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,30 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
+
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		@user.update user_params
+		if @user.save
+			redirect_to "/users/#{@user.id}"
+		else
+			flash[:errors] = ['Invalid']
+			redirect_to :back
+		end
+
 		
 	end
 
 	private
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :bio)
 	end
+	# def update_params
+	# 	params.require(:user).permit()
+	# end
+
 end
