8027dddade5ec48b50667a0aa99b600d3342d5f5
dcgong93 <dcgong93@gmail.com>
2016-06-28 16:41:15 -0700

    discussion scrollTop-bot, added some links

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @create = Comment.new comment_params
     @create.save
-    redirect_to dashboards_main_path
+    redirect_to '/dashboards/main#bottom'
   end
 
   private
