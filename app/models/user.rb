455721a23db47075b836e13e0945d0d5170d92dc
Danny Nguyen <danhn142@gmail.com>
2016-06-29 09:31:30 -0700

    Pushing the most updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_attached_file :avatar, :styles => {:thumb => "100x100>"} 
+  has_attached_file :avatar 
   has_many :comments
   has_many :matches, :foreign_key => 'user_id', :class_name => "Match"
   has_many :opponents, through: :matches
@@ -10,4 +10,10 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :first_name, :last_name, :email, presence:true, length: { in: 2..20 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
+before_save :default_values
+  def default_values
+    self.bio ||= 'Tell us about yourself.'
+  end
+
 end
