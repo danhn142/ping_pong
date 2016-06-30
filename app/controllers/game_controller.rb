3315411aeabc0cf97ea8c3da6b2198c931e2c4a9
dcgong93 <dcgong93@gmail.com>
2016-06-29 18:00:27 -0700

    matches create/destroy/accept/deny

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,5 @@
-class DashboardsController < ApplicationController
-  def main
-    @players = User.all.order('rank')
-    @comments = Comment.all.order('created_at')
+class GameController < ApplicationController
+  def show
     @challenge = Match.where(user: current_user, opponent: User.where(rank: current_user.rank-1), status: "pending")
     @pending = Match.where(user: User.where(rank: current_user.rank+1), opponent: current_user, status: "pending").first
     @challenger = User.where(rank: current_user.rank+1).first
